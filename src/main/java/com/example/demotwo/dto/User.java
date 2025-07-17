64fe37de1ccd20c938c282929a02b14b978fb645
Anurag Patil <73098953+Anurag-cod4fun@users.noreply.github.com>
2025-07-17 20:49:16 +0530

    Update User.java

--- src/main/java/com/example/demotwo/dto/User.java
+++ src/main/java/com/example/demotwo/dto/User.java
@@ -1,8 +1,27 @@
 package com.example.demotwo.dto;
+import java.io.Serializable;
 
-public class User {
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String id;
     private String name;
+
+    public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 
     public User() {}
 
@@ -10,6 +29,4 @@ public class User {
         this.id = id;
         this.name = name;
     }
-
-    // Getters and setters
 }
